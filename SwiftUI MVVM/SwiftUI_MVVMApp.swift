72d767c96c397cba28f9703d359ed1a558f6fb5b
Joao Leonardo Pereira <joaoleonardo_pereira@apple.com>
2024-01-19 15:20:26 -0300

    Initial Commit

---
+++ SwiftUI MVVM/SwiftUI_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_MVVMApp.swift
+//  SwiftUI MVVM
+//
+//  Created by Joao Leonardo Pereira on 19/01/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
